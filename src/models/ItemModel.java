dbf08333ea2498ca4431f44a3cf8ef7f76aece2b
Matt <navx2810@gmail.com>
2014-11-30 22:17:09 -0500

    added the ItemModel

---
+++ src/models/ItemModel.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.io.Serializable;
+
+import abstracts.AbstractModel;
+import entities.Item;
+
+public class ItemModel extends AbstractModel<Item> implements Serializable 
+{
+
+	private static final long serialVersionUID = 5089924843405219836L;
+	
+}
