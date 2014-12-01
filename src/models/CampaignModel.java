e7b7d485b4a33ba58a107b3c0f30e2499ccba89b
Matt <navx2810@gmail.com>
2014-11-30 20:07:58 -0500

    fleshed out the CampaignModel

--- src/models/CampaignModel.java
+++ src/models/CampaignModel.java
@@ -1,5 +1,25 @@
 package models;
 
-public class CampaignModel {
+import java.io.Serializable;
 
+public class CampaignModel implements Serializable 
+{
+	private EventLogModel eventLogModel;
+	private PlayerModel playerModel;
+	
+	public CampaignModel() 
+	{
+		eventLogModel = new EventLogModel();
+		playerModel = new PlayerModel();
+	}
+	
+	public EventLogModel getEventLogModel() 
+	{
+		return eventLogModel;
+	}
+	
+	public PlayerModel getPlayerModel() 
+	{
+		return playerModel;
+	}
 }
